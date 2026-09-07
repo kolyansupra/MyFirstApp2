1adcec11e444fce100a911f009a5787a305947dd
SupraImp <nikolaynikolaev777@mail.ru>
2026-09-07 20:44:24 +1000

    Initial Commit

---
+++ MyFirstApp/MyFirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstAppApp.swift
+//  MyFirstApp
+//
+//  Created by Николай on 07.09.2026.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
